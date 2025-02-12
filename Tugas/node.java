6660bccf348c8d53d7b03c461490575923f3a0d2
igas1234 <fabregasandro@gmail.com>
2025-02-13 00:05:47 +0700

    Tugas Pertemuan 2

---
+++ Tugas/node.java
@@ -0,0 +1,11 @@
+public class node {
+    private int nilai;
+    private Node next;
+
+    //inisialisasi  Node
+    public Node(int nilai){
+        this.nilai = nilai;
+    }
+
+    //setter dan getter
+}
